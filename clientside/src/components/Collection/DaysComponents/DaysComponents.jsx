5a321e661317e48fae7e78e6c79fe29980b80ca2
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-03 07:55:55 +0100

    Add DaysComponents.jsx

---
+++ clientside/src/components/Collection/DaysComponents/DaysComponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// internal import
+import Style from "./DaysComponents.module.css";
+const DaysComponents = () => {
+  return (
+    <div>DaysComponents</div>
+  )
+};
+
+export default DaysComponents;
