81e96d9694b5e5626317aaca24882aefe2c7323e
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-03 12:02:59 +0100

    Update SearchBar.jsx

--- clientside/src/SearchPage/SearchBar/SearchBar.jsx
+++ clientside/src/SearchPage/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BsSearch, BsArrowRight} from 'react-icons/bs';
+import { BsSearch, BsArrowRight } from 'react-icons/bs';
 import Style from './SearchBar.module.css';
 
 const SearchBar = ({ onSearch }) => {
@@ -15,22 +15,23 @@ const SearchBar = ({ onSearch }) => {
         }
     };
 
-    
     return (
         <div className={Style.SearchBar}>
             <div className={Style.SearchBar_box}>
-             <input
-                 type="text"
-                 value={query}
-                 onChange={handleInputChange}
-                 placeholder="Search NFTs..."
-                 className={Style.searchInput}
-             />
-            <BsSearch onClick={handleSearch} className={Style.SearchBar_box_icon}/>
-
+                <input
+                    type="text"
+                    value={query}
+                    onChange={handleInputChange}
+                    placeholder="Search NFTs..."
+                    className={Style.searchInput}
+                />
+                <BsSearch
+                    onClick={handleSearch}
+                    className={`${Style.SearchBar_box_icon} ${query ? Style.active : ''}`}
+                />
             </div>
         </div>
-    )
+    );
 };
 
 export default SearchBar;
