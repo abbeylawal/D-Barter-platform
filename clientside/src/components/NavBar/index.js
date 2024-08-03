92172d87a232b2b22108d615a2fc4da03f81e248
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-03 13:56:19 +0100

    Update index.js

--- clientside/src/components/NavBar/index.js
+++ clientside/src/components/NavBar/index.js
@@ -3,5 +3,6 @@ import Notification from './Notification/Notification';
 import SideBar from './SideBar/SideBar';
 import Discover from './Discover/Discover';
 import HelpCenter from './HelpCenter/HelpCenter';
+import ToggleTheme from './ToggleTheme/ToggleTheme';
 
-export { Discover, HelpCenter, Notification, Profile, SideBar };
+export { Discover, HelpCenter, ToggleTheme, Notification, Profile, SideBar };
