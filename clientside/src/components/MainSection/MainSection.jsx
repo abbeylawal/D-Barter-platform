1b09a74b7bb12cab63450619eeafc4adfd78ffa5
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-03 07:58:50 +0100

    Update MainSection.jsx

--- clientside/src/components/MainSection/MainSection.jsx
+++ clientside/src/components/MainSection/MainSection.jsx
@@ -9,14 +9,17 @@ const MainContent = () => {
         <div className={Style.mainsection}>
             <div className={Style.mainsection_box}>
                 <div className={Style.mainsection_box_left}>
-                    <h1>Discover 🔍, collect 📦 and Trade NFTs 🔄 </h1>
+                    <h1>Create 🐼 and Trade NFTs 🔄 </h1>
                     <p>
                         Dive into the exciting world of digital assets, where you can explore unique creations, gather your favorite collectibles, and engage in seamless trading with others. NFTs (Non-Fungible Tokens) offer a revolutionary way to own and exchange digital content, ensuring authenticity and ownership through blockchain technology.
                     </p>
                     <Button btnName="Start your search" handleClick={() => { }} />
                 </div>
                 <div className={Style.mainsection_box_right}>
-                    <Image src={images.hero} alt="Main Section" width={600} height={600} />
+                    <Image src={images.hero} alt="Main Section" 
+                    width={600} 
+                    height={600} 
+                    />
                 </div>
             </div>
         </div>
