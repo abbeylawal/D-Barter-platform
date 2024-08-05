b731ad9f284b83bf54dd13f42c9b8b53c32df3c3
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-05 06:09:34 +0100

    Update Banner.jsx

--- clientside/src/components/Banner/Banner.jsx
+++ clientside/src/components/Banner/Banner.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Style from './Banner.module.css';
+import Image from 'next/image';
 
 const Banner = ({ bannerImage }) => {
     return (
-        <div className={Style.banner} style={{ backgroundImage: `url(${bannerImage})` }}>
-            <div className={Style.overlay}></div>
+        <div className={Style.banner}>
+            <div className={Style.banner_img}>
+                <Image 
+                    src={bannerImage}
+                    alt="background image"
+                    width={1600}
+                    height={200}
+                    objectFit='cover'
+                />
+            </div>
+            {/* <div className={Style.banner_img_mobile}>
+                <Image 
+                    src={bannerImage}
+                    alt="background image"
+                    width={1600}
+                    height={900}
+                    objectFit='cover'
+                />
+
+            </div> */}
             {/* <div className={Style.content}>
                 <h1>Welcome to Our NFT Marketplace</h1>
                 <p>Discover, create, and sell extraordinary NFTs</p>
