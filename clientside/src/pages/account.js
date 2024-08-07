f31d1159a0798a2902c0c6631b01954dfc38f137
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-07 06:59:35 +0100

    Update account.js

--- clientside/src/pages/account.js
+++ clientside/src/pages/account.js
@@ -1,56 +1,50 @@
 import Image from "next/image";
 import { useState, useCallback, useContext, useMemo } from "react";
-import React from 'react';
-import Style from"../styles/account.module.css";
-import {useDropzone} from 'react-dropzone';
+import React from "react";
+import Style from "../styles/account.module.css";
+import { useDropzone } from "react-dropzone";
 
+import images from "../asserts/img";
 
-import images from "../img";
-import ProfileForm from "../AccountPage/ProfileForm/ProfileForm";
+import ProfileForm from "./AccountPage/ProfileForm/ProfileForm";
 
 const account = () => {
-    const [fileUrl, setFileUrl] = useState(null);
+  const [fileUrl, setFileUrl] = useState(null);
 
-    const onDrop = useCallback(async(acceptedFile) => {
-        setFileUrl(acceptedFile[0]);
+  const onDrop = useCallback(async (acceptedFile) => {
+    setFileUrl(acceptedFile[0]);
+  }, []);
 
-    }, []);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+    maxSize: 5000000,
+  });
 
-    const {getRootProps, getInputProps} = useDropzone({
-        onDrop,
-        accept: "image/*",
-        maxSize: 5000000,
-    });
-
-    return (
-        <div className={Style.account}>
-            <div className={Style.account_info}>
-                <h1>Profile Settings</h1>
-                <p>
-
-                </p>
-
-            </div>
-            <div className={Style.account_box}>
-                <div className={Style.account_box_img} {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    <Image
-                        src={images.user1}
-                        alt="account upload"
-                        width={150}
-                        height={150}
-                        className={Style.account_box_img_img}
-                    />
-                    <p className={Style.account_box_img_p}>
-                        Change Image
-                    </p>
-                </div>
-                <div className={Style.account_box_from}>
-                    <ProfileForm />
-                </div>
-            </div>
+  return (
+    <div className={Style.account}>
+      <div className={Style.account_info}>
+        <h1>Profile Settings</h1>
+        <p></p>
+      </div>
+      <div className={Style.account_box}>
+        <div className={Style.account_box_img} {...getRootProps()}>
+          <input {...getInputProps()} />
+          <Image
+            src={images.user1}
+            alt="account upload"
+            width={150}
+            height={150}
+            className={Style.account_box_img_img}
+          />
+          <p className={Style.account_box_img_p}>Change Image</p>
         </div>
-    )
+        <div className={Style.account_box_from}>
+          <ProfileForm />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default account;
