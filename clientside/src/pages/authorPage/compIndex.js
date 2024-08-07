c7aebddb24ab73cab3644560913643f7dcb260cc
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-07 06:59:27 +0100

    Add compIndex.js

---
+++ clientside/src/pages/authorPage/compIndex.js
@@ -0,0 +1,6 @@
+import AuthorProfileCard from "./AuthorProfileCard/AuthorProfileCard";
+import AuthorTaps from "./AuthorTaps/AuthorTaps";
+import TabCard from "./TabCard/TabCard";
+
+
+export { AuthorProfileCard, AuthorTaps, TabCard }
