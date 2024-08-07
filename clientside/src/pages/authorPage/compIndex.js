250e1331167fbafca0fcb6cd2ffde668bb91301d
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-07 12:17:03 +0100

    Update compIndex.js

--- clientside/src/pages/authorPage/compIndex.js
+++ clientside/src/pages/authorPage/compIndex.js
@@ -1,6 +1,6 @@
 import AuthorProfileCard from "./AuthorProfileCard/AuthorProfileCard";
-import AuthorTaps from "./AuthorTaps/AuthorTaps";
-import TabCard from "./TabCard/TabCard";
+import AuthorTabs from "./AuthorTabs/AuthorTabs";
+import AuthorNFTCard from "./AuthorNFTCard/AuthorNFTCard";
 
 
-export { AuthorProfileCard, AuthorTaps, TabCard }
+export { AuthorProfileCard, AuthorTabs, AuthorNFTCard };
