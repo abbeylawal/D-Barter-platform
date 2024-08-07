75f4c139016c53636bdc2cbcb58fbbca4c8dfff5
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-07 12:16:53 +0100

    Add AuthorNFTCard.jsx

---
+++ clientside/src/pages/authorPage/AuthorNFTCard/AuthorNFTCard.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import Style from "./AuthorNFTCard.module.css";
+import images from "../../../asserts/img";
+import { NFTCard } from '../../../components/componentsIndex';
+
+const AuthorNFTCard = ({ collectables, created, listed, like }) => {
+
+    return (
+        < div className={Style.AuthorNFTCard}  >
+            {collectables && <NFTCard />}
+            {created && <NFTCard />}
+            {listed && <NFTCard />}
+            {like && <NFTCard />}
+        </div>
+    )
+}
+
+export default AuthorNFTCard;
