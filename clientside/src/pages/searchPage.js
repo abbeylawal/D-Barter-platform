7375f55236753a880770f87b164bd444f0b50c85
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-07 06:59:38 +0100

    Update searchPage.js

--- clientside/src/pages/searchPage.js
+++ clientside/src/pages/searchPage.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 import Style from "../styles/searchPage.module.css";
-import { NFTSlider, Filter, Banner} from "../components/componentsIndex";
-import { SearchBar } from '../SearchPage/searchPageIndex';
-import { NFTCardTwo } from '../components/componentsIndex';
-import { NFTCard } from '../components/componentsIndex';
-import images from "../img";
+import { NFTSlider, Filter, Banner } from "../components/componentsIndex";
+import { SearchBar } from "./SearchPage/searchPageIndex";
+import { NFTCardTwo } from "../components/componentsIndex";
+import { NFTCard } from "../components/componentsIndex";
+import images from "../asserts/img";
 // import NFTCardTwo from "../collectionPage/collectionIndex";
 const searchPage = () => {
   return (
     <div className={Style.searchPage}>
-        <Banner bannerImage={images.creatorbackground2} />
-        <SearchBar />
-        <Filter />
-        <NFTCard />
-        <NFTCardTwo />
-        <NFTSlider />
-
+      <Banner bannerImage={images.creatorbackground2} />
+      <SearchBar />
+      <Filter />
+      <NFTCard />
+      <NFTCardTwo />
+      <NFTSlider />
     </div>
-  )
+  );
 };
 
 export default searchPage;
