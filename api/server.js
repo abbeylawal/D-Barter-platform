3435aa16ef36c76968f1dd714ff16c2d6d4fdc6e
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-11 05:27:11 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 
+// console.log(app.get("env"));
+// console.log(process.env);
+
 const port = 3000;
 const host = "127.0.0.1"; // specify your custom host
 app.listen(port, host, () => {
