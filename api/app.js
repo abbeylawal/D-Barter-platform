0d7e59a1fb343a6f04f9aa82b1fdb79f014cee72
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-11 05:27:10 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json()); // middleware express function
 app.use(morgan("dev"));
 
+//Custom Template
+app.use(express.static(`${__dirname}/nft-data/img`));
+
 // ============ MIDDLEWARE ================
 app.use((req, res, next) => {
     console.log("Hey I am from Middleware ")
