b3b7ed4c9ae191b87c54dce1922fadfd1588d171
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-11 05:27:13 +0100

    Update Category.jsx

--- clientside/src/components/Category/Category.jsx
+++ clientside/src/components/Category/Category.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { BsCircleFill } from "react-icons/bs";
-import images from "../../asserts/img";
 import Image from "next/image";
 import Style from "./Category.module.css";
+import images from "../../assets/img";
 
 const Category = () => {
-    // const CategoryArray = [1,2,3,4,5,6];
-    const CategoryArray = [
-        images.creatorbackground1,
-        images.creatorbackground10,
-        images.creatorbackground2,
-        images.creatorbackground11,
-        images.creatorbackground4,
-        images.creatorbackground5];
+    const categories = [
+        { image: images.creatorbackground1, name: 'Fashion', count: '1995 NFTS' },
+        { image: images.creatorbackground10, name: 'Electronics', count: '320 NFTS' },
+        { image: images.creatorbackground2, name: 'Computers', count: '1780 NFTS' },
+        { image: images.creatorbackground11, name: 'Gadgets', count: '250 NFTS' },
+        { image: images.creatorbackground4, name: 'Mobiles', count: '500 NFTS' },
+        { image: images.creatorbackground5, name: 'Arts', count: '1340 NFTS' }
+    ];
 
     return (
         <div className={Style.box_category}>
             <div className={Style.category}>
-                {CategoryArray.map((el, i) => (
-                    <div className={Style.category_box}>
+                {categories.map((category, i) => (
+                    <div className={Style.category_box} key={i}>
                         <Image
-                            src={el}
+                            src={category.image}
                             className={Style.category_box_img}
-                            alt='creator Background'
+                            alt={`${category.name} Background`}
                             width={350}
                             height={150}
                             objectFit="cover"
@@ -33,15 +33,15 @@ const Category = () => {
                                 <BsCircleFill />
                             </span>
                             <div className={Style.category_box_title_info}>
-                                <h4> Entrainment </h4>
-                                <small>1995 NFTS</small>
+                                <h4>{category.name}</h4>
+                                <small>{category.count}</small>
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
-    )
+    );
 };
 
 export default Category;
