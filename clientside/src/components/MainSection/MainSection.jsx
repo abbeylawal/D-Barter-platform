571b3739c70227019442fe2b4334015f44f8b049
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-11 05:27:13 +0100

    Update MainSection.jsx

--- clientside/src/components/MainSection/MainSection.jsx
+++ clientside/src/components/MainSection/MainSection.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Style from './MainSection.module.css';
 import Image from 'next/image';
 import { Button } from "../componentsIndex";
-import images from "../../asserts/img";
+import images from "../../assets/img";
+import { NFTMarketplaceContext } from "../../../SmartContract/Context/NFTMarketplaceContext";
 
 const MainContent = () => {
+    const { titleData } = useContext(NFTMarketplaceContext);
     return (
         <div className={Style.mainsection}>
             <div className={Style.mainsection_box}>
                 <div className={Style.mainsection_box_left}>
-                    <h1>Create 🐼 and Trade NFTs 🔄 </h1>
+                    {/* <h1>Create 🐼 and Trade NFTs 🔄 </h1> */}
+                    <h1>{ titleData }</h1>
                     <p>
                         Dive into the exciting world of digital assets, where you can explore unique creations, gather your favorite collectibles, and engage in seamless trading with others. NFTs (Non-Fungible Tokens) offer a revolutionary way to own and exchange digital content, ensuring authenticity and ownership through blockchain technology.
                     </p>
