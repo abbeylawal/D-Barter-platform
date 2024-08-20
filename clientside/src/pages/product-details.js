ee0a5776e7b6abb15809db4d105225406318f106
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-20 07:31:02 +0100

    Update product-details.js

--- clientside/src/pages/product-details.js
+++ clientside/src/pages/product-details.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import {useRouter} from "next/router";
 import Style from "../styles/product-details.module.css";
 import { Button, Category } from "../components/componentsIndex";
 // import { ProductDescription, ProductDetailsImg, ProductTabs } from "./ProductDetailsPage/ProductDetailsIndex";
 import ProductDetailsPage from "./ProductDetailsPage/ProductDetailsPage";
 
+
+// SMART CONTRACT
+import { NFTMarketplaceContext } from "../../SmartContract/Context/NFTMarketplaceContext";
+
 const product_details = () => {
+  const { currentAccount } = useContext(NFTMarketplaceContext);
+  
+  const [nft, setNft] = useState({
+    image: "",
+    tokenId: "",
+    name: "",
+    owner: "",
+    seller: "",
+  });
+  
+  const router = useRouter();
+  useEffect(() => {
+    if (!router.isReady) return;
+    setNft(router.query);
+  }, [router.isReady]);
   return (
     <div className={Style.product_details}>
-      <ProductDetailsPage />
+      <ProductDetailsPage nft={nft} />
       <Category />
     </div>
   );
