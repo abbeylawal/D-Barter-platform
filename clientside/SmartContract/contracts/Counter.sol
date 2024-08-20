eccc8ab3f465f3e9e0dceb6ace01bf20a5961900
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-20 07:30:52 +0100

    Update Counter.sol

--- clientside/SmartContract/contracts/Counter.sol
+++ clientside/SmartContract/contracts/Counter.sol
@@ -6,20 +6,27 @@ library CounterLib {
         uint256 _value; // default: 0
     }
 
+    // Returns the current value of the counter
     function current(Counter storage counter) internal view returns (uint256) {
         return counter._value;
     }
 
+    // Increments the counter by 1
     function increment(Counter storage counter) internal {
-        counter._value += 1;
+        unchecked {
+            counter._value += 1;
+        }
     }
 
+    // Decrements the counter by 1 with an underflow check
     function decrement(Counter storage counter) internal {
-        uint256 value = counter._value;
-        require(value > 0, "Counter: decrement overflow");
-        counter._value = value - 1;
+        require(counter._value > 0, "Counter: decrement overflow");
+        unchecked {
+            counter._value -= 1;
+        }
     }
 
+    // Resets the counter to 0
     function reset(Counter storage counter) internal {
         counter._value = 0;
     }
