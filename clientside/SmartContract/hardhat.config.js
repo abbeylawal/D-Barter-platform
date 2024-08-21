f18bf4a3abb5b9bcc0fb0175a4535ef1dc0a344d
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-21 06:18:04 +0100

    Update hardhat.config.js

--- clientside/SmartContract/hardhat.config.js
+++ clientside/SmartContract/hardhat.config.js
@@ -1,11 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-// module.exports = {
-//   solidity: "0.8.24",
-// };
-
-
 module.exports = {
   solidity: "0.8.24",
   networks: {
@@ -13,10 +8,5 @@ module.exports = {
       chainId: 1337,
     },
   }
-  // paths: {
-  //   sources: "./contracts",
-  //   tests: "./test",
-  //   cache: "./cache",
-  //   artifacts: "./artifacts"
-  // }
+
 };
