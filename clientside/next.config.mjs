6bd87b5a6ec05056f9970f78c44680d5a801db28
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-21 06:18:05 +0100

    Update next.config.mjs

--- clientside/next.config.mjs
+++ clientside/next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        // domains: ["subdomain", "infura-ipfs.io"]
-        domains: ['gateway.pinata.cloud'],
-    },
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    PINATA_API_KEY: process.env.PINATA_API_KEY,
+    PINATA_SECRET_API_KEY: process.env.PINATA_SECRET_API_KEY,
+  },
+  images: {
+    // domains: ["subdomain", "infura-ipfs.io"]
+    domains: ["gateway.pinata.cloud"],
+  },
 };
 
 export default nextConfig;
