a2936b28baec3fab28a51a22a97e806d42e17e1f
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-23 20:15:35 +0100

    Update NavBar.jsx

--- clientside/src/components/NavBar/NavBar.jsx
+++ clientside/src/components/NavBar/NavBar.jsx
@@ -39,51 +39,16 @@ const NavBar = () => {
             <div className={Style.navbar_container}>
                 {/* navbar left */}
                 <div className={Style.navbar_container_left}>
-                    {/* <div className={Style.logo}>
-                        <Image
-                            src={images.logo}
-                            alt='BARTER MARKET PLACE'
-                            href="/"
-                            width={100}
-                            height={100} />
-                    </div> */}
-
-                    <a className="navbar-brand flex-shrink-3" href="/"
-                    style={{ position: "relative", zIndex: 9999999999, marginRight: "1rem" }}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2009/svg"
-                            viewBox="0 0 250 100"
-                            className="img-fluid"
-                            width="100"
-                        >
-                            <circle cx="50" cy="50" r="45" fill="var(--icons-color)" />
-                            <path d="M30 50 L50 30 L70 50 L50 70 Z" fill="white" />
-                            <path d="M40 50 L50 40 L60 50 L50 60 Z" fill="var(--main-bg-color)" />
-                            <text
-                                x="110"
-                                y="50"
-                                fontFamily="Arial, sans-serif"
-                                fontSize="38"
-                                fontWeight="bold"
-                                fill="var(--icons-color)"
-                            >
-                                Barter
-                            </text>
-                            <text
-                                x="110"
-                                y="75"
-                                fontFamily="Arial, sans-serif"
-                                fontSize="26"
-                                fontWeight="bold"
-                                fill="var(--icons-color)"
-                            >
-                                Easy
-                            </text>
-                        </svg>
-                        {/* <span>Barter Easy Store</span> */}
-
-                    </a>
+                    <div className={Style.logo}>
+                        <a href="/" style={{ cursor: 'pointer' }}>
+                            <Image
+                                src={images.logo}
+                                alt='BARTER MARKET PLACE'
+                                width={100}
+                                height={100}
+                            />
+                        </a>
+                    </div>
                     <div className={Style.navbar_container_left_box_input}>
                         <div className={Style.navbar_container_left_box_input_box}>
                             <input type="text" placeholder="Search Item" />
