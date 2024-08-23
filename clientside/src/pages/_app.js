fb61dac332807aac61cb29ce28b86e856c76c857
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-08-23 20:15:36 +0100

    Update app.js

--- clientside/src/pages/_app.js
+++ clientside/src/pages/_app.js
@@ -1,20 +1,24 @@
 import "../styles/global.css";
-
+import { useRouter } from "next/router";
 import { NavBar } from "../components/componentsIndex";
 import { Footer } from "../components/componentsIndex";
 import { BackToTopButton } from "../components/componentsIndex";
 import { NFTMarketplaceProvider } from "../../SmartContract/Context/NFTMarketplaceContext";
 
-const MyApp = ({ Component, pageProps }) => (
-    <div>
-        <NFTMarketplaceProvider>
-            <NavBar/>
-            <Component {...pageProps} />
-            <BackToTopButton />
-            <Footer />
-        </NFTMarketplaceProvider>
-    </div>
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const showFooter = router.pathname !== "/auth";
 
-);
+  return (
+    <div>
+      <NFTMarketplaceProvider>
+        <NavBar />
+        <Component {...pageProps} />
+        <BackToTopButton />
+        {showFooter && <Footer />}
+      </NFTMarketplaceProvider>
+    </div>
+  );
+};
 
 export default MyApp;
