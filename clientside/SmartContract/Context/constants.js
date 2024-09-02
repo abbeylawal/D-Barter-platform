bb24982684390ecacfad55e11d37420ef56430a9
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-09-02 20:39:01 +0100

    Update constants.js

--- clientside/SmartContract/Context/constants.js
+++ clientside/SmartContract/Context/constants.js
@@ -1,6 +1,12 @@
-// 0xe7f1725e7734ce288f8367e1bb143e90bb3f0512;
 
 import nftMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
 
-export const NFTMarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+import htlc from "../artifacts/contracts/HTLC.sol/HTLC.json";
+
+
+export const NFTMarketplaceAddress = "0x7748D46F9233801f516EE5e1B21Ed6cc6b4495F0";
 export const NFTMarketplaceABI = nftMarketplace.abi;
+
+
+export const HTLCAddress = "0xB35Cb75E5EA4d39DAd4D1dE8710FdB17779e6fe9";
+export const HTLC_ABI = htlc.abi;
