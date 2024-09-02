aadc5a5d4e72dc479d6b6b3e482ad0314d6c09a7
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-09-02 20:39:10 +0100

    Update index.js

--- clientside/src/pages/index.js
+++ clientside/src/pages/index.js
@@ -26,34 +26,38 @@ const Home = () => {
   //   checkWalletConnection();
   // }, []);
 
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const handleCategoryChange = (category) => {
+    setActiveCategory(category);
+  };
   return (
     <div>
-        <div className={Style.homePage}>
-
-            <MainSection/>
-            {/* <Title
+      <div className={Style.homePage}>
+        <MainSection />
+        {/* <Title
               heading="New Collection"
               paragraph="Explore the NFT in the most featured categories."
             /> */}
-           <br/>
-            <Title
-              heading="Top Listed Barters"
-              paragraph="Explore the NFT in the most featured categories."
-            />
-            <NFTSlider />
-            {/* <Collection /> */}
-            <Title
-              heading="Featured Items"
-              paragraph="Explore creators products in the most featured categories."
-            />
-            <Filter />
-            <NFTCard />
-            <Title
-              heading="Browse by Category"
-              paragraph="Explore the NFT in the most featured categories."
-            />
-            <Category />
-        </div>
+        <br />
+        <Title
+          heading="Top Listed Barters"
+          paragraph="Explore the NFT in the most featured categories."
+        />
+        <NFTSlider />
+        {/* <Collection /> */}
+        <Title
+          heading="Featured Items"
+          paragraph="Explore creators products in the most featured categories."
+        />
+        <Filter onCategoryChange={handleCategoryChange} />
+        <NFTCard activeCategory={activeCategory} />
+        <Title
+          heading="Browse by Category"
+          paragraph="Explore the NFT in the most featured categories."
+        />
+        <Category />
+      </div>
     </div>
   );
 };
