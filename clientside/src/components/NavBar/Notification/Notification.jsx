0080b327d8fe75ec49ca7e35ed6d9641ef6c1b26
Abdullahi Abiodun Lawal <lawalabbeysmith@gmail.com>
2024-09-02 20:39:07 +0100

    Update Notification.jsx

--- clientside/src/components/NavBar/Notification/Notification.jsx
+++ clientside/src/components/NavBar/Notification/Notification.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Image from 'next/image';
 import Style from "./Notification.module.css";
 import images from "../../../assets/img";
+import userData from "../../../assets/Data/userData.json";
+
+
+import { NFTMarketplaceContext } from "../../../../SmartContract/Context/NFTMarketplaceContext";
 
 const Notification = () => {
+
+    // Smart Contract Section
+    const { currentAccount, connectWallet, openError } = useContext(NFTMarketplaceContext);
+
+    // Ensure currentAccount exists and has userId
+    const userId = currentAccount ? currentAccount.userId : 1;
+    const user = userData[userId]
+
     return (
         <div className={Style.notification}>
             <p>Notification</p>
             <div className={Style.notification_box}>
                 <div className={Style.notification_box_img}>
-                    <Image src={images.user1} alt="profile image"
+                    <Image src={user.userImage} alt="profile image"
                         width={50}
                         height={50}
                         style={{ borderRadius: '50%' }}
                     />
                 </div>
                 <div className={Style.notification_box_info}>
-                    <h4>Muftau Lawal</h4>
-                    <p>Measure action your..</p>
+                    <h4>{user.name} </h4>
+                    <p>Offer on your product..</p>
                     <small>3 minutes ago</small>
                 </div>
                 <span className={Style.notification_box_new}>
