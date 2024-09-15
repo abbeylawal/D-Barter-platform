1d3e9428ff444dc534117444db06ac5fad2f58d7
abbeylawal-pol <abdullahi.lawal@polarisdigitech.com>
2024-09-15 18:14:26 +0100

    Update hardhat.config.js

--- clientside/SmartContract/hardhat.config.js
+++ clientside/SmartContract/hardhat.config.js
@@ -1,17 +1,27 @@
+// hardhat.config.js
+
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 50,
+      },
+    },
+  },
   networks: {
     hardhat: {
       chainId: 1337,
     },
-    //  ganache test net
+    // Ganache Testnet
     ganache: {
       url: process.env.GANACHE_TESTNET_RPC_URL,
-      accounts: [process.env.GANACHE_PRIVATE_KEY]
+      accounts: [process.env.GANACHE_PRIVATE_KEY],
     },
   },
 };
