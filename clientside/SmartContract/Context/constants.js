ef531812b9edb7f17b71064237c717b65da01824
abbeylawal-pol <abdullahi.lawal@polarisdigitech.com>
2024-09-17 02:03:24 +0100

    Update constants.js

--- clientside/SmartContract/Context/constants.js
+++ clientside/SmartContract/Context/constants.js
@@ -4,9 +4,9 @@ import nftMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketp
 import htlc from "../artifacts/contracts/HTLC.sol/HTLC.json";
 
 
-export const NFTMarketplaceAddress = "0xE4dE42799f3cbb901ACCDc01D790427F56eBC02C";
+export const NFTMarketplaceAddress = "0x2C9afeA1b3C66EaE38F79999908063AB196e20dB";
 export const NFTMarketplaceABI = nftMarketplace.abi;
 
 
-export const HTLCAddress = "0xa6Ff9E0fF171FC9Ba4727080FC3aE290Ef38084E";
+export const HTLCAddress = "0xB177B85A7811383b213508a9532D56AdAea2574E";
 export const HTLC_ABI = htlc.abi;
